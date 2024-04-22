091b3f889e365274e53a921794cbfcf65a891795
Gabriel Fray <gabrielfraygarandy@gmail.com>
2024-04-21 21:15:04 -0300

    feat: globalstyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout";
-import GlobalStyles from "@/styles/GlobalStyles";
 
 import "@/styles/swiperStyles.css";
+import GlobalStyles from "@/styles/GlobalStyles";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
